e95abe7095baaf48eff66bac3984d420b8bbb83d
Dat Duong <datd1988@gmail.com>
2017-11-26 19:06:21 -0800

    Create downloadLinks.py

---
+++ getAbstract/downloadLinks.py
@@ -0,0 +1,16 @@
+import re
+fin = open ("/u/flashscratch/d/datduong/pubmedAbstractBaseLineYearly/index.html",'r')
+script = "" 
+for line in fin: 
+  path = re.findall("href=.+\"",line)
+  if len(path)==0:
+    continue
+  path = re.sub ( "href=\"","",path[0] )
+  path = re.sub ( "\"","",path )
+  if "gz.md5" in path: 
+    continue
+  script = script + "wget " + path + " -O /u/flashscratch/d/datduong/pubmedAbstractBaseLineYearly/baseline \n"
+
+fout = open ("/u/flashscratch/d/datduong/pubmedAbstractBaseLineYearly/download.sh",'w')
+fout.write(script)
+fout.close() 
