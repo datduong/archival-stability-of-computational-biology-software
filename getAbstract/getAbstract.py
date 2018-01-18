1480db39f5545da7f16a51382d0752341398bd57
Dat Duong <datd1988@gmail.com>
2018-01-17 21:41:26 -0800

    Update getAbstract.py

--- getAbstract/getAbstract.py
+++ getAbstract/getAbstract.py
@@ -1,66 +1,22 @@
-
-
-
 from urllib2 import HTTPError
 import xml.etree.ElementTree
-import os,re, sys, httplib
+import os,re,sys
+import argparse
+import httplib
 from urlparse import urlparse
 
-
-def isNodeTag (node,nodeTag): ## Breadth-first search approach. 
-  queue = [node]
-  if node.tag == nodeTag: 
-    return node, node.text 
-  if len(node.getchildren())==0: 
-    return "tag+not+found"
-  queue.remove(node)
-  queue = queue + node.getchildren()
-  while len(queue)>0: ## not empty 
-    node = queue[0] ## node to be removed 
-    queue.remove(node)
-    if node.tag == nodeTag: 
-      return node
-    if len(node.getchildren())>0: ## add more nodes to "probe" 
-      queue = queue + node.getchildren()
-  return "tag+not+found"
+from urlXmlUtil import * 
   
+ 
+paperName = "/u/flashscratch/d/datduong/pubmedArticle4Dec17/journalWanted/Bioinformatics/PMC2530882.nxml" 
+getHttpStatus(paperName,'abstract')
 
-def getAbstract (paperName,genericFunc,**kwargs):
-  argList = {'Year':0,'ISOAbbreviation':'none'}
-  for key in kwargs: 
-    if key not in argList: 
-      print key + ' option not supported' 
-      continue
-    argList[key] = kwargs[key]
-  ##
-  tree = xml.etree.ElementTree.parse(paperName)
-  for node in tree.iter():
-    if node.tag == "PubmedArticle" :
-      try: 
-        DateCreated = isNodeTag(node,"DateCreated") ## @DateCreated is a node in the xml, gets time when paper was completed
-        year = isNodeTag(DateCreated,"Year").text     
-        date = isNodeTag(DateCreated,"Day").text
-        month = isNodeTag(DateCreated,"Month").text
-        ArticleTitle = isNodeTag(node,"ArticleTitle").text ## paper title 
-        Abstract = isNodeTag(node,"Abstract")
-        AbstractText = isNodeTag(Abstract,"AbstractText").text ## abstract text (may be limited if over 400 words)
-        Journal = isNodeTag(node,"Journal")
-        JournalTitle = isNodeTag(Journal,"Title").text 
-        JournalAbbrev = isNodeTag(Journal,"ISOAbbreviation").text
-      except: 
-        continue ## something not found. like Journal not found/ Abstract not found etc...
-      ### filter the output 
-      if argList['Year']!=0:
-        if int(year) != argList['Year']:
-          continue
-      if argList['ISOAbbreviation']!='none':
-        if JournalAbbrev != argList['ISOAbbreviation']:
-          continue
-      ## do something to @AbstractText with @genericFunc generic function 
-      thisPaperOutcome = genericFunc(AbstractText)
+## see outpt in the format
+## PubmedId, Year, KeyWord related to link, Link, LinkStatus, StatusCode. 
+## '18628289 2008 download http://www.systemsbiology.co.kr/PathCluster/ -1 '
 
-    
-paperName = "/u/flashscratch/d/datduong/pubmedAbstractBaseLineYearly/baseline/medline17n0305.xml" 
-
-getAbstract(paperName)
-
+## Status code: 
+# -1 is Connection Timeout 
+# 200-299: working link
+# 300-399: redirected link 
+# 400 and over: broken link
